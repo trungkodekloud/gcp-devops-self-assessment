c146fab8973fb0b3cf77fc4cdfa5064118306b8b
terraform+1-at-067637015277 <onye71@gmail.com>
2023-08-17 14:51:26 -0400

    whatever chages

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<div style=\"text-align: center;\"><h1>Flask App KodeKloud!</h1><p>Welcome Adina.</p><p></p></div>'
+    return '<div style=\"text-align: center;\"><h1>Flask Kloud!</h1><p>Welcome Adina.</p><p></p></div>'
 
 
 if __name__ == "__main__":
